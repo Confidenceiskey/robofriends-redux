d6de29b57e2e2f37f39bfc41d16c452bfa2d2423
David N <bigair_1080@hotmail.com>
2019-06-27 19:57:11 -0300

    updated favicon, manifest & added label

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,9 +3,11 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange}) => {
 	return (
 		<div style={{marginBottom: '20px'}}>
+      <label for='search' />
 			<input 
       className='pa3 ba b--green bg-lightest-blue'
 			type='search' 
+      id='search'
 			placeholder='Search robots' 
       onChange={searchChange}
       />
